eb1afdb418b2dbcd07a003d05f21d9f0388aa984
MotaZorrilla <80704587+MotaZorrilla@users.noreply.github.com>
2021-03-20 18:34:15 -0400

    Add files via upload

---
+++ technews/main.js
@@ -0,0 +1,9 @@
+
+document.querySelector('.menu-btn').addEventListener('click',() => {
+    document.querySelector('.nav-menu').classList.toggle("show");
+}); 
+
+ScrollReveal().reveal('.showcase',{delay: 1000});
+ScrollReveal().reveal('.news-cards',{delay: 1000});
+ScrollReveal().reveal('.cards-banner1',{delay: 1000});
+ScrollReveal().reveal('.cards-banner1',{delay: 1000});
